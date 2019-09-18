1454c55f89c39ab59638e01873a0d7cf35fb73f2
thebigsteph <nzali.stephane86@gmail.com>
2019-09-18 18:31:43 +0100

    add script file and HTML form

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(id, title, author, numberOfPages, alreadyRead=false) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.alreadyRead = alreadyRead;
+  }
+
+function addBookToLibrary() {
+  this.Book()
+}
