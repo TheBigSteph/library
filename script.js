8609bfe04d7dcb6d3abe291e36f6ecd2b1293c53
Dan Montiel <danmontielh@gmail.com>
2019-09-18 18:16:15 -0500

    add render function to display all books

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let myLibrary = [];
+let myLibrary = [{title: '100 Años de Soledad', author: 'Gabriel García Márquez', numberOfPages: 471, alreadyRead: true}, 
+{title: 'El Túnel', author: 'Ernesto Sábato', numberOfPages: 471, alreadyRead: true},
+{title: 'El Coronel no tiene quien le escriba', author: 'Ernesto Sábato', numberOfPages: 471, alreadyRead: true}];
 
-function Book(id, title, author, numberOfPages, alreadyRead=false) {
-    this.id = id;
+function Book( title, author, numberOfPages, alreadyRead=false) {
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
@@ -11,3 +12,37 @@ function Book(id, title, author, numberOfPages, alreadyRead=false) {
 function addBookToLibrary() {
   this.Book()
 }
+
+function render(){
+  
+   let node = document.getElementById('library');
+   myLibrary.forEach(book => {
+    node.appendChild(addDiv(book.title));
+    let newDiv = document.getElementById(book.title);
+    AddInfo(newDiv,book)
+   });
+ 
+}
+
+function AddInfo(div, book){
+  div.innerHTML = `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Title Book: ${book.title}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Author: ${book.author}</h6>
+    <p class="card-text">Pages: ${book.numberOfPages}</p>
+    <p class="card-text">Read: ${book.alreadyRead}</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>`;
+  
+}
+
+function addDiv(id= ""){
+  let createDiv = document.createElement('div');
+  createDiv.className = "col-sm-3";
+  createDiv.id = id;
+  return createDiv;
+}
+
+render();
