c4e62abecaaa19298797102554aca8a6baa48783
thebigsteph <nzali.stephane86@gmail.com>
2019-09-28 00:55:42 +0100

    add design

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let myLibrary = JSON.parse(localStorage.getItem('library')) || [{title: '100 Años de Soledad', author: 'Gabriel García Márquez', numberOfPages: 471, alreadyRead: 'Read'}, 
 {title: 'El Túnel', author: 'Ernesto Sábato', numberOfPages: 471, alreadyRead: "Unread"},
 {title: 'El Coronel no tiene quien le escriba', author: 'Ernesto Sábato', numberOfPages: 471, alreadyRead: "Read"}];
-// localStorage.setItem('library', JSON.stringify(myLibrary));
 
-const BookAdd = document.getElementById('add-book')
+const BookAdd = document.getElementById('add-book');
 
 function Book( title, author, numberOfPages, alreadyRead="Unread") {
     this.title = title;
@@ -11,6 +10,13 @@ function Book( title, author, numberOfPages, alreadyRead="Unread") {
     this.numberOfPages = numberOfPages;
     this.alreadyRead = alreadyRead;
   }
+
+ 
+
+function setLocalStorage() {
+  localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+
 
 function addBookToLibrary() {
   resetDivbyId('library');
@@ -20,9 +26,6 @@ function addBookToLibrary() {
   const read = document.getElementById('read').value;
   const createBook = new Book(title, author, pages, read);
   myLibrary.push(createBook);
-  localStorage.setItem('library', JSON.stringify(myLibrary));
-
-
 // CODE Stephane
   // const title = $('#title').val();
   // const author = $('#author').val();
@@ -31,27 +34,20 @@ function addBookToLibrary() {
   // myLibrary.push(bk);
   // console.log(myLibrary);
   resetForm('form-book');
+  $('#bookModal').modal('hide')
   render();
 }
 
 function removeBookFromLibrary(index) {
-  console.log(index);
-  const items = JSON.parse(localStorage.getItem("library"));
-  for (var i =0; i< items.length; i++) {
-    const item = JSON.parse(items[i]);
-    if (i == index) {
-        item.splice(i, 1);
-    }
-}
-items = JSON.stringify(items);
-localStorage.setItem("library", items);
-  // resetDivbyId('library');
+  myLibrary.splice(index,1);
+  resetDivbyId('library');
   render();
 }
-
 function editBookFromLibrary(index) {
-  myLibrary[index].alreadyRead == 'Unread' ? myLibrary[index].alreadyRead = 'Read': myLibrary[index].alreadyRead = 'Unread';
-  resetDivbyId('library');
+  // myLibrary[index].alreadyRead == 'Unread' ? myLibrary[index].alreadyRead = 'Read': myLibrary[index].alreadyRead = 'Unread';
+  let data = myLibrary[index];
+  data.alreadyRead == 'Unread' ? data.alreadyRead = 'Read' : data.alreadyRead = 'Unread';
+  // resetDivbyId('library');
   render();
 }
 
@@ -69,42 +65,45 @@ function resetDivbyId(nameId) {
 // }
 
 function render(){
-  
+  setLocalStorage();
    let node = document.getElementById('library');
-   console.log(node.childNodes);
-   locale = JSON.parse(localStorage.getItem('library'));
-   locale.forEach((book, index) => {
+   let dataBookStorage = JSON.parse(localStorage.getItem('library'));
+   console.log(dataBookStorage);
+   dataBookStorage.forEach((book, index) => {
     node.appendChild(addDiv(book.title));
     let newDiv = document.getElementById(book.title);
-    AddInfo(newDiv,book, index)
-    
+    AddInfo(newDiv,book, index);
    });
  
 }
 
 function AddInfo(div, book, index){
-  div.innerHTML = `<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Title Book: ${book.title}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Author: ${book.author}</h6>
-    <p class="card-text">Pages: ${book.numberOfPages}</p>
-    <p class="card-text">Read: ${book.alreadyRead} <span><a href="#" class="card-link" onclick="editBookFromLibrary(${index});">Edit</a></span></p>
-    <a href="#" class="card-link" onclick="removeBookFromLibrary(${index});">Delete Book</a>
-  </div>
-</div>`;
-  
+  div.innerHTML = `
+    <div class="card mb-4 shadow-sm">
+      <div class="card-body">
+        <h3 class="card-title text-primary"> ${book.title}</h3>
+        <h6 class="card-subtitle mb-2 text-muted">Author: ${book.author}</h6>
+        <p class="card-text">Pages: ${book.numberOfPages}</p>
+        <p class="card-text">${book.alreadyRead}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" onclick="editBookFromLibrary(${index});" class="btn btn-sm btn-primary">Edit book</button>
+            <button type="button" onclick="removeBookFromLibrary(${index});" class="btn btn-sm btn-danger">Delete book</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
 }
 
 
 
 function addDiv(id= ""){
   let createDiv = document.createElement('div');
-  createDiv.className = "col-sm-3";
+  createDiv.className = "col-md-4";
   createDiv.id = id;
   return createDiv;
 }
-
-
 
 
 render();
